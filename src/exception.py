1866f40e2bd0fdc56d9cc82f9520449e09e128c7
Wuraola Mathew <arena6663@gmail.com>
2025-03-15 15:25:59 +0100

    EDA and model training files

--- src/exception.py
+++ src/exception.py
@@ -1,25 +1,19 @@
-import sys  # crucial for system interactions, error handling, and debugging.
-import logging
+import sys
+from src.logger import logging 
 
-# Configure logging to show messages in console
-logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
-
-
-def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb= error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message= "Error occur in python script name [{0}] line number [{1}] error message[{2}]".format(
-    file_name,exc_tb.tb_lineno, str(error)
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
     )
     return error_message
 
 class CustomException(Exception):
-    def  __init__(self, error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message= error_message_detail(error_message, error_detail=error_detail)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
     
     def __str__(self):
         return self.error_message
-    
-
 
